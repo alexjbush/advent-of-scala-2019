3a0c2ea374b828445b563896c9f4dfe31994df22
Bush <alexbush@kpmg.co.nz>
2019-12-05 10:22:23 +1300

    Part1

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -1,22 +1,36 @@
 object Day3 {
 
+  def main(args: Array[String]): Unit = {
+
+    val input = io.Source.fromResource("day3.txt").getLines().toList
+
+    println(s"Part1: ${findClosestCrossingPoint(input.head, input.tail.head)}")
+
+  }
+
   def generatePositions(input: List[String]): List[(Int, Int)] = {
     input
       .map(v => (v.head, v.tail.toInt))
-      .scanLeft((0, 0)){
-        case ((i, j), ('U', c)) => (i + c, j)
-        case ((i, j), ('D', c)) => (i - c, j)
-        case ((i, j), ('R', c)) => (i, j + c)
-        case ((i, j), ('L', c)) => (i, j - c)
+      .scanLeft(((0, 0), (0, 0))) {
+        case ((_, (i, j)), ('U', c)) => ((i, j), (i + c, j))
+        case ((_, (i, j)), ('D', c)) => ((i, j), (i - c, j))
+        case ((_, (i, j)), ('R', c)) => ((i, j), (i, j + c))
+        case ((_, (i, j)), ('L', c)) => ((i, j), (i, j - c))
       }
+      .flatMap {
+        case ((i1, j1), (i2, _)) if i1 != i2 => (i1 to i2 by (math.signum(i2 - i1))).map((_, j1))
+        case ((i1, j1), (_, j2)) if j1 != j2 => (j1 to j2 by (math.signum(j2 - j1))).map((i1, _))
+        case (v, _) => List(v)
+      }
+
   }
 
-  def findClosestCrossingPoint(input1: List[String], input2: List[String]): Int = {
-    generatePositions(input1)
+  def findClosestCrossingPoint(input1: String, input2: String): Int = {
+    generatePositions(input1.split(',').toList)
       .toSet
-      .intersect(generatePositions(input2).toSet)
-      .filter(_ != (0,0))
-      .map{ case (i, j) => i + j}
+      .intersect(generatePositions(input2.split(',').toList).toSet)
+      .filter(_ != (0, 0))
+      .map { case (i, j) => i.abs + j.abs }
       .min
   }
 
--- src/test/scala/TestDay3.scala
+++ src/test/scala/TestDay3.scala
@@ -3,17 +3,23 @@ import org.scalatest.matchers.should.Matchers
 
 class TestDay3 extends AnyFunSpec with Matchers {
 
-  describe("generatePositions"){
-    it("First example"){
-      Day3.generatePositions(List("R8","U5","L5","D3")) should contain theSameElementsInOrderAs(List((0,0), (0,8), (5,8), (5,3), (2,3)))
+  describe("generatePositions") {
+    it("First example") {
+      Day3.generatePositions(List("R8", "U5", "L5", "D3")) should contain theSameElementsInOrderAs (
+        List(
+          (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7),
+          (0, 8), (1, 8), (2, 8), (3, 8), (4, 8), (5, 8), (5, 9),
+          (5, 8), (5, 7), (5, 6), (5, 5), (5, 4), (5, 3), (6, 3),
+          (5, 3), (4, 3), (3, 3), (2, 3))
+        )
     }
   }
 
-  describe("findClosestCrossingPoint"){
-    it("Second example"){
-      val input1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(',').toList
-      val intput2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(',').toList
-      Day3.findClosestCrossingPoint(input1, intput2) should be (135)
+  describe("findClosestCrossingPoint") {
+    it("Second example") {
+      val input1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+      val intput2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+      Day3.findClosestCrossingPoint(input1, intput2) should be(135)
     }
   }
 
